0635c99263706a51a1ec30ca81644b4c141b6530
Felipe <felipedelucanogueira@gmail.com>
2021-03-30 22:24:47 -0300

    Widget CardContainer criado

--- lib/main.dart
+++ lib/main.dart
@@ -7,17 +7,41 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Container(child: Column(
-      children:[Expanded(child: Container()),
-        Container(color: Colors.blue,
-          width: MediaQuery.of(context).size.width * 1,
-          height: MediaQuery.of(context).size.height * 0.15,
-        )
-      ],
-    ),);
+    return Container(
+      child: Column(
+        children: [
+          Expanded(
+              child: Container(
+            margin: EdgeInsets.all(10),
+            child: Column(
+              children: [
+                CardContainer(),
+                CardContainer(),
+                CardContainer(),
+              ],
+            ),
+          )),
+          Container(
+            color: Colors.blue,
+            width: MediaQuery.of(context).size.width * 1,
+            height: MediaQuery.of(context).size.height * 0.15,
+          )
+        ],
+      ),
+    );
   }
 }
 
+class CardContainer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(20),
+      color: Colors.white,
+      width: MediaQuery.of(context).size.width * 0.85,
+      height: MediaQuery.of(context).size.height * 0.15,
+    );
+  }
+}
